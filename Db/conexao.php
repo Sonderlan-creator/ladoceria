89077d93783226b6884b6ddcc76de29be8d358cc
Midaxs <ildsonjr2akt@gmail.com>
2025-05-25 22:00:11 -0300

    pouca coisa no lugar

---
+++ Db/conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "auth-db1660.hstgr.io";
+$username = "u182528050_ladoceria";
+$password = '@sSNwx4s$W&AW?A6';
+$dbname = "u182528050_ladoceria";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// if ($conn->connect_error) {
+//     die("Conexão falhou: " . $conn->connect_error);
+// }
+// else {
+//      echo "Conexão bem-sucedida!";
+// }
+?>
