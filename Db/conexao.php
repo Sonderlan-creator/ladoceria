3e8a12b2047b398748c3a1132caf74e3ac8b5669
Ed <edcarlosmottamelo@gmail.com>
2025-06-02 21:52:23 -0300

    Update conexao.php

--- Db/conexao.php
+++ Db/conexao.php
@@ -1,15 +1,15 @@
 <?php
 // Configuração para servidor remoto (descomente para usar em produção)
-// $servername = "auth-db1660.hstgr.io";
-// $username = "u182528050_ladoceria";
-// $password = '@sSNwx4s$W&AW?A6';
-// $dbname = "u182528050_ladoceria";
+$servername = "auth-db1660.hstgr.io";
+$username = "u182528050_ladoceria";
+$password = '@sSNwx4s$W&AW?A6';
+$dbname = "u182528050_ladoceria";
 
 // Configuração para localhost (descomente para usar localmente)
-$servername = "localhost";
-$username = "root";
-$password = '';
-$dbname = "u182528050_ladoceria";
+// $servername = "localhost";
+// $username = "root";
+// $password = '';
+// $dbname = "u182528050_ladoceria";
 
 // Cria conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -18,5 +18,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-
 ?>
